f2aba5d0f4086832230740dfc55cffd153d3b0dd
raiibrar <128582137+raiibrar@users.noreply.github.com>
2023-12-04 06:51:21 +0000

    configuration of Envirnment

---
+++ test.js
@@ -0,0 +1,22 @@
+"use strict"
+
+//console.log("Abrar")
+
+let score = 33
+console.log(score);
+//console.log(typeof(score))
+ score = String ("33")
+ console.log(typeof(score));
+
+ let isactive = true
+// console.log(isactive)
+isactive = Number(1)
+
+//console.log(isactive)
+
+//isactive= null
+//console.log(isactive)
+//isactive = undefined
+//console.log(typeof(isactive))
+isactive = null
+console.log(typeof(isactive));
